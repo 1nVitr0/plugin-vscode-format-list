fc71051ca9e7f9315a944a4e9bf60875943714b0
Aram Becker <aram@konsolenkost.de>
2024-08-07 09:38:05 +0200

    l10n: add translations for output target names

--- src/providers/formats/index.ts
+++ src/providers/formats/index.ts
@@ -1,3 +1,4 @@
+import { l10n } from "vscode";
 import { DefaultFormatterLanguages } from "../../types/Formatter";
 import ListFormatProvider from "./ListFormatProvider";
 import { formatC } from "./c";
@@ -16,20 +17,20 @@ import { formatXml } from "./xml";
 import { formatYaml } from "./yaml";
 
 export const listFormatProviders: Record<DefaultFormatterLanguages, ListFormatProvider> = {
-  csv: new ListFormatProvider("CSV (Default Options)", formatCsvDefault),
-  csvCustom: new ListFormatProvider("CSV (Custom Options)", formatCsvCustom),
-  json: new ListFormatProvider("JSON", formatJson),
-  jsonl: new ListFormatProvider("JSON Log (Newline separated JSON objects)", formatJsonLog),
-  yaml: new ListFormatProvider("YAML", formatYaml),
-  xml: new ListFormatProvider("XML", formatXml),
-  textList: new ListFormatProvider("Separated list", formatTextList),
-  sql: new ListFormatProvider("SQL", formatSqlInsert),
-  sqlUpdate: new ListFormatProvider("SQL Update", formatSqlUpdate),
-  markdown: new ListFormatProvider("Markdown", formatMarkdown),
-  javascript: new ListFormatProvider("JavaScript", formatJavaScript),
-  php: new ListFormatProvider("PHP Array", formatPhp),
-  c: new ListFormatProvider("C Array", formatC),
-  cpp: new ListFormatProvider("C++ Array", formatCpp),
-  java: new ListFormatProvider("Java Array", formatJava),
-  latex: new ListFormatProvider("LaTeX", formatLatex),
+  csv: new ListFormatProvider(l10n.t("CSV (Default Options)"), formatCsvDefault),
+  csvCustom: new ListFormatProvider(l10n.t("CSV (Custom Options)"), formatCsvCustom),
+  json: new ListFormatProvider(l10n.t("JSON"), formatJson),
+  jsonl: new ListFormatProvider(l10n.t("JSON Log (Newline separated JSON objects)"), formatJsonLog),
+  yaml: new ListFormatProvider(l10n.t("YAML"), formatYaml),
+  xml: new ListFormatProvider(l10n.t("XML"), formatXml),
+  textList: new ListFormatProvider(l10n.t("Separated list"), formatTextList),
+  sql: new ListFormatProvider(l10n.t("SQL Insert"), formatSqlInsert),
+  sqlUpdate: new ListFormatProvider(l10n.t("SQL Update"), formatSqlUpdate),
+  markdown: new ListFormatProvider(l10n.t("Markdown"), formatMarkdown),
+  javascript: new ListFormatProvider(l10n.t("JavaScript"), formatJavaScript),
+  php: new ListFormatProvider(l10n.t("PHP Array"), formatPhp),
+  c: new ListFormatProvider(l10n.t("C Array"), formatC),
+  cpp: new ListFormatProvider(l10n.t("C++ Array"), formatCpp),
+  java: new ListFormatProvider(l10n.t("Java Array"), formatJava),
+  latex: new ListFormatProvider(l10n.t("LaTeX"), formatLatex),
 };
